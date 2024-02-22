aa6274733438f909259a36be3734c2e1d710944c
Carmen Morata <cmoratagarcia@gmail.com>
2024-02-22 11:02:20 -0800

    Set up initial objects as per project instructions

---
+++ script.js
@@ -0,0 +1,19 @@
+//store the gameboard as an array inside of a Gameboard object
+function Gameboard() {
+  let board = [];
+
+  
+}
+
+//Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
+function gamePlay() {}
+const players = [
+  {
+    name: playerOneName,
+    token: "X",
+  },
+  {
+    name: playerTwoName,
+    token: "0",
+  },
+];
