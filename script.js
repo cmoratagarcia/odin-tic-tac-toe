969cf29f62895413f5916ce6afee92bb5ca39902
Carmen Morata <cmoratagarcia@gmail.com>
2024-02-22 15:13:05 -0800

    Update createBoard function

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 //store the gameboard as an array inside of a Gameboard object
-function Gameboard(rows, columns) {
-  let board = [];
-
+const gameBoard = (() => {
+  const board = [];
+  const getBoard = () => board;
   //Add clear function
 
-  for (let i = 0; i < rows; i++) {
-    board[i] = [];
-    for (let j = 0; j < columns; j++) {
-      board[i].push(createCell(j)); //This will eventually be a button for each cell
+  function createBoard(rows, columns) {
+    for (let i = 0; i < rows; i++) {
+      board[i] = [];
+      for (let j = 0; j < columns; j++) {
+        board[i].push(board[i][j]); //This will eventually be a button for each cell
+      }
     }
   }
+  const checkCell = (row, column) => {
+    if (board[row][column] === undefined) {
+      return true;
+    }
+  };
 
-  function checkWin(board) {
+  function checkWin() {
     for (let i = 0; i < board.length; i++) {
       if (
-        board[i][0] != " " &&
+        board[i][0] != " " && //Change to undefined? or use inner HTML when buttons
         board[i][0] === board[i][1] &&
         board[i][0] === board[i][2]
       ) {
@@ -36,17 +43,17 @@ function Gameboard(rows, columns) {
       return true;
     }
     if (
-      board[0][2] != " " &&
-      board[0][2] === board[1][1] &&
-      board[0][2] === board[2][0]
+      board[2][0] != " " &&
+      board[2][0] === board[1][1] &&
+      board[2][0] === board[0][2]
     ) {
       return true;
     }
     return false;
   }
 
-  return board; //return other board functions
-}
+  return { getBoard, createBoard, checkCell, checkWin }; //return other board functions
+})();
 
 //Your players are also going to be stored in objects, and you’re probably going to want an object to control the flow of the game itself.
 // function gamePlay() {}
@@ -67,14 +74,14 @@ function Gameboard(rows, columns) {
 // //   },
 // ];
 
-function createCell(index) {
-  let cellToken = "";
-  const markCell = (token) => {
-    cellToken = token;
-  };
-  const getToken = () => cellToken;
+// function createCell(index) {
+//   let cellToken = "";
+//   const markCell = (token) => {
+//     cellToken = token;
+//   };
+//   const getToken = () => cellToken;
 
-  return { index, markCell, getToken };
-}
-
-console.log(Gameboard(3, 3).length);
+//   return { index, markCell, getToken };
+// }
+gameBoard.createBoard(3, 3);
+console.log(gameBoard.getBoard());
